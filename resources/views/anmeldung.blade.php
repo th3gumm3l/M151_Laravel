9581006a42d19e20a28dd890217628d1efbd3bcb
Tim Bernhard <tim.bernhard@me.com>
2023-03-01 16:07:34 +0100

    Anmelde view erstellt und die Route dazu

---
+++ resources/views/anmeldung.blade.php
@@ -0,0 +1,32 @@
+<!doctype html>
+    <html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Anmeldung</title>
+</head>
+<body>
+    <h1>Anmeldung</h1>
+    <form action="?" method="Post">
+        <p>
+            E-Mail:
+            <input type="email" name="email" id="email" placeholder="E-mail">
+        </p>
+        <p>
+            Name:
+            <input type="text" name="name" id="name" placeholder="Name">
+        </p>
+        <p>
+            Vorname:
+            <input type="text" name="vorname" id="vorname" placeholder="Vorname">
+        </p>
+        <p>
+            Bemerkungen:
+            <input type="text" name="bemerkungen" id="bemerkungen" placeholder="Bemerkungen">
+        </p>
+        <input type="submit" value="Anmelden">
+    </form>
+</body>
+</html>
